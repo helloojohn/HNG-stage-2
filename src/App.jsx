0664c727b3abd167db5a8e948b61bfb46b37d477
John Okorie <ameachijohn001@gmail.com>
2023-09-12 15:43:14 +0100

    TMBD API test

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,40 @@
-// import bg from './assets/bg-image.png'
-function App() {
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
-  const bgStyle = {
-    backgroundColor: '#1aac48',
-  }
+const MovieList = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const apiKey = import.meta.env.VITE_APP_TMDB_API_KEY; // Replace with your TMDb API key
+    const apiUrl = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}&language=en-US&page=1`;
+
+    axios.get(apiUrl)
+      .then(response => {
+        setMovies(response.data.results);
+        // console.log(movies)
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
+  }, [movies]);
+
+  // console.log(movies)
+
   return (
-    <>
-      <header style={bgStyle}>
-        HELLO
-      </header>
-    </>
-  )
+    <div>
+      <h2>Popular Movies</h2>
+      <ul>
+        {movies.map(movie => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default MovieList;
+
+
+
+
+
